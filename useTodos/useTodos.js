24f5640f6b0556b374d3dfcdc187800eb761bb0e
FJAP96x <lx02918@innovacion.mx>
2024-09-26 18:06:33 -0600

    create folder and add useTodos

--- useTodos/useTodos.js
+++ useTodos/useTodos.js
@@ -1,20 +1,20 @@
-import { todoReducer } from "../08-useReducer/TodoReducer";
-import { useReducer, useEffect } from "react";
+import { todoReducer } from './TodoReducer';
+import { useReducer, useEffect } from 'react';
 
 export const useTodos = () => {
   const initialState = [];
 
   const init = () => {
-    return JSON.parse(localStorage.getItem("todos")) || [];
+    return JSON.parse(localStorage.getItem('todos')) || [];
   };
 
   const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
   const handleNewTodo = (todo) => {
     const action = {
-      type: "[TODO] Add Todo",
+      type: '[TODO] Add Todo',
       payload: todo,
     };
     dispatchTodo(action);
@@ -22,14 +22,14 @@ export const useTodos = () => {
 
   const handleDeleteTodo = (id) => {
     dispatchTodo({
-      type: "[TODO] Remove Todo",
+      type: '[TODO] Remove Todo',
       payload: id,
     });
   };
 
   const handleToggleTodo = (id) => {
     dispatchTodo({
-      type: "[TODO] Toggle Todo",
+      type: '[TODO] Toggle Todo',
       payload: id,
     });
   };
